59ac5f6404a885a1c7d5eb85725f08f994553e30
xiongmeijuan <hzxmj@tairanchina.com>
2018-01-31 19:58:00 +0800

    update

---
+++ tpls/es6/exportimport/a.js
@@ -0,0 +1,9 @@
+// export function fun(){
+//   console.log('I am a function')
+// }
+const a = { 
+  b: {
+    c: 4
+  } 
+}
+export default a
---
+++ tpls/es6/exportimport/es5/a.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// export function fun(){
+//   console.log('I am a function')
+// }
+var a = {
+  b: {
+    c: 4
+  }
+};
+exports.default = a;
